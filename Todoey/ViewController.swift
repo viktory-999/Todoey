0ff43c69fafbf1be667741893c0d6534ac827cc6
Виктория Ширяева <viktoriasiraeva@macbook-air-viktoria.local>
2018-12-19 17:51:03 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Виктория Ширяева on 19.12.18.
+//  Copyright © 2018 Виктория Ширяева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
